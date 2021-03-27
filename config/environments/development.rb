8b85b5dbbe03649638717d8908145487cbdcfe24
alpha-blog app <nikhileshkumar2509@gmail.com>
2021-03-27 12:14:12 +0530

    add mailgun and modified development and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.mailgun_settings = {
+    api_key: '09187d1c54d541aa360ff86b01bb4c3c-1553bd45-e73e98b3',
+    domain: 'photo-app-nikhilesh.herokuapp.com',
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
---
+++ config/initializers/mailgun.rb
@@ -0,0 +1,3 @@
+Mailgun.configure do |config|
+    config.api_key = '09187d1c54d541aa360ff86b01bb4c3c-1553bd45-e73e98b3'
+end
